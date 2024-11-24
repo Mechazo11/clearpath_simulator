db436f3aa6026218f2d96610b8486f035d1e385d
Mechazo11 <azmyin12@gmail.com>
2024-11-24 10:28:24 -0600

    added teleop_twist_joy node into simulation.launch file, works

--- clearpath_gz/launch/simulation.launch.py
+++ clearpath_gz/launch/simulation.launch.py
@@ -21,12 +21,16 @@ Modified by
 """
 
 # Imports
+import os
+from launch.actions import LogInfo
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+
+
 # Declare launch arguments
 ARGUMENTS = [
     DeclareLaunchArgument('rviz', default_value='false',
@@ -41,8 +45,17 @@ ARGUMENTS = [
                           description='use_sim_time'),
     DeclareLaunchArgument('robot_config_yaml',
                           default_value='robot.yaml',
-                          description='Default name of a robot`s configuration file name')
+                          description='Default name of a robot`s configuration file name'),
+    DeclareLaunchArgument('joy_config', default_value='xbox',
+                          description='Joystick configuration to use'),
+    DeclareLaunchArgument('joy_dev', default_value='0',
+                          description='Joystick device'),
+    DeclareLaunchArgument('publish_stamped_twist', default_value='false',
+                          description='Publish geometry_msgs/TwistStamped instead of geometry_msgs/Twist'),
+    DeclareLaunchArgument('config_filepath', default_value = ''), # The path is resolved during runtime
+    DeclareLaunchArgument('config_with_yaml', default_value=[LaunchConfiguration('joy_config'), '.config.yaml'])
 ]
+
 # Set robot pose
 for pose_element in ['x', 'y', 'yaw']:
     ARGUMENTS.append(DeclareLaunchArgument(pose_element, default_value='0.0',
@@ -57,11 +70,16 @@ def generate_launch_description():
     pkg_clearpath_gz = get_package_share_directory(
         'clearpath_gz')
 
-    # Join paths to additional launch files to spawn world and robot
+    # Join paths to additional launch files to spawn world, robot, joy controller
     gz_sim_launch = PathJoinSubstitution(
         [pkg_clearpath_gz, 'launch', 'gz_sim.launch.py'])
+    
     robot_spawn_launch = PathJoinSubstitution(
         [pkg_clearpath_gz, 'launch', 'robot_spawn.launch.py'])
+    
+    # Construct full path to the teleop-launch.py file located in teleop_twist_joy
+    teleop_twist_joy_launch = PathJoinSubstitution(
+        [get_package_share_directory('teleop_twist_joy'), 'launch', 'teleop-launch.py'])
 
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([gz_sim_launch]),
@@ -84,8 +102,25 @@ def generate_launch_description():
             ('yaw', LaunchConfiguration('yaw'))]
     )
 
+    # ROS 2 resolves dynamic launch during runtime. Hence only in this part, we can resolve the full path to `xbox.config.yml` file
+    joy_config_yaml_string = PathJoinSubstitution([get_package_share_directory('teleop_twist_joy'), 'config', LaunchConfiguration('config_with_yaml')])
+    
+    # Use LogInfo to print the resolved path
+    log_action = LogInfo(msg=joy_config_yaml_string)
+
+    teleop_twist_joy_spawn = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([teleop_twist_joy_launch]),
+        launch_arguments=[
+            ('joy_config', LaunchConfiguration('joy_config')),
+            ('joy_dev', LaunchConfiguration('joy_dev')),
+            ('publish_stamped_twist', LaunchConfiguration('publish_stamped_twist')),
+            ('config_filepath', joy_config_yaml_string)
+        ]
+    )
     # Create launch description and add actions
-    ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(gz_sim)
-    ld.add_action(robot_spawn)
+    ld = LaunchDescription(ARGUMENTS) 
+    # ld.add_action(log_action) # To print debug message
+    ld.add_action(teleop_twist_joy_spawn)
+    #ld.add_action(gz_sim)
+    #ld.add_action(robot_spawn)
     return ld
